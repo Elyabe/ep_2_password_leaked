299e36653d5c9249697f81c519abe317a1140a97
elyabe <elly.ufes@gmail.com>
2019-07-05 19:53:11 -0300

    Versão com mensagem única

--- main.cpp
+++ main.cpp
@@ -22,115 +22,84 @@ int main(int argc, char *argv[])
     
     if ( argc != 2 )
     {
-        printf("Bad leck :(! \n");
+        printf("Bad leck :(! Qual eh a senha \n");
         exit(-1);
     }
 
     /* code */
-    char caracteres[] = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", pwd_decrypt[255];
+    char caracteres[] = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", pwd_decrypt[80], senha[TAM_MAX+1];
     // char caracteres[] = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789", pwd_decrypt[255];
     strcpy(pwd_decrypt,argv[1]);
     
     const long CH_LEN = strlen(caracteres);
-    char sc[TAM_MAX+1];
-    
-    int  rank, size, L = 1, done = -1;
-    bool achou, res;
+    int  rank, size, TAM_SENHA, RECEBEU_MSG = 0;
+    bool achou = false;
 
     MPI_Status stats;
-    MPI_Request req;
+    MPI_Request requisicao_msg;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
     MPI_Comm_size(MPI_COMM_WORLD, &size); 
-
-    res = achou = false;
+    
 
     if ( rank == ROOT )
     {
-        while( !res )
-        {
-            if(done != 0)
+
+        // Aguarda mensagem de qualquer processo (o que achar primeiro)
+        MPI_Irecv(&senha, TAM_MAX, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &requisicao_msg);
+
+        while( !RECEBEU_MSG )
+            MPI_Test(&requisicao_msg, &RECEBEU_MSG, &stats);
+
+
+        printf("Processo : %d ==: senha %s \n", stats.MPI_SOURCE, senha);
+
+        const double tempoSeq = (MyClock()-inicio)/CLOCKS_PER_SEC;
+        cout << fixed << setprecision(25);
+        // cout << "  Valor esperado: " << M_PI << endl;
+        // cout << "   Erro Relativo: " << fabs(x - M_PI)/M_PI << endl;
+        printf("     Tempo total: %.5lf\n", tempoSeq);
+        // cout << "   SpeedUp: " << 90.4/tempoSeq << endl;
+        // cout << "   Eficiencia: " << 100*(90.4/tempoSeq)/16 << endl;
+
+        MPI_Abort(MPI_COMM_WORLD, 0);
+
+    } else {
+            long long y[5], q, CH_LEN_POT[TAM_MAX+1], QTD_POSSIBILIDADES;
+
+            for ( TAM_SENHA = 1; !achou && TAM_SENHA <= TAM_MAX; ++TAM_SENHA )
             {
-                MPI_Irecv(&res, 1, MPI_C_BOOL, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &req);
-                done = 0;
-            }
-                MPI_Test(&req, &done, &stats);
-
-                if (done != 0) { 
-                    MPI_Recv(&sc, TAM_MAX, MPI_CHAR, stats.MPI_SOURCE, TAG, MPI_COMM_WORLD, &stats);
-                    
-                    printf("Processo : %d ==: senha %s \n", stats.MPI_SOURCE, sc);
-
-                    double tempoSeq = (MyClock()-inicio)/CLOCKS_PER_SEC;
-                    cout << fixed << setprecision(25);
-                    // cout << "  Valor esperado: " << M_PI << endl;
-                    // cout << "   Erro Relativo: " << fabs(x - M_PI)/M_PI << endl;
-                    printf("     Tempo total: %.5lf\n", tempoSeq);
-                    // cout << "   SpeedUp: " << 90.4/tempoSeq << endl;
-                    // cout << "   Eficiencia: " << 100*(90.4/tempoSeq)/16 << endl;
-
-                    done = -1;
-                    MPI_Abort(MPI_COMM_WORLD, 0);
-                    break;
-                }                
-
-        } 
-
-    } else 
-    {
-            long y[5], q, CH_LEN_POT[TAM_MAX+1];
-            long long QTD_POSSIB;
-
-            for ( L = 1; !achou && L <= TAM_MAX; L++ )
-            {
-                QTD_POSSIB = (long long)pow( strlen(caracteres), L);
+                QTD_POSSIBILIDADES = (long long)pow( CH_LEN, TAM_SENHA);
                 
+                // Define os valores para calculos de indices necessarios o tamanho atual de senha
                 for ( q = 1; q <= TAM_MAX; q++ )
-                    CH_LEN_POT[q] = (long)pow(CH_LEN, L-q);
-                
-                // CH_LEN_POT[1] = (long)pow(CH_LEN, L-1);
-                // CH_LEN_POT[2] = (long)pow(CH_LEN, L-2);
-                // CH_LEN_POT[3] = (long)pow(CH_LEN, L-3);
-                // CH_LEN_POT[4] =  CH_LEN;
-                // CH_LEN_POT[5] =  1;
+                    CH_LEN_POT[q] = (long)pow(CH_LEN, TAM_SENHA-q);
 
-                for ( long i = rank-1; i < QTD_POSSIB ; i += size-1)
+                // Analisa todas as possibilidades
+                for ( long long i = rank-1; i < QTD_POSSIBILIDADES ; i += size-1)
                 {
-                    // Define os indices i,j,k,l
+                    // Define apenas os indices necessarios para o tamanho da senha calculada
                     y[0] = i;
-                    for ( q = 1 ; q < L; ++q )
+                    for ( q = 1 ; q < TAM_SENHA; ++q )
                         y[q] = y[q-1] % CH_LEN_POT[q];
 
                     // Forma a palavra
-                    for ( q = 0 ; q < L; ++q )
-                        sc[q] = caracteres[y[q] / CH_LEN_POT[q+1]];    
-                    sc[q] = '\0';
+                    for ( q = 0 ; q < TAM_SENHA; ++q )
+                        senha[q] = caracteres[y[q] / CH_LEN_POT[q+1]];    
+                    senha[q] = '\0';
 
 
-                    if ( !strcmp( sha1(sc).c_str(), pwd_decrypt) )
+                    if ( !strcmp( sha1(senha).c_str(), pwd_decrypt) )
                     {
-                        // printf("%lf : Processo %d achou %s com L = %d \n", (MyClock()-inicio)/CLOCKS_PER_SEC,  rank, sc, L );
                         achou = true;
-
-                        // Informa ao raiz que achou
-                        MPI_Send(&achou, 1, MPI_C_BOOL, ROOT, TAG, MPI_COMM_WORLD); 
-                        // Envia a senha descriptografada
-                        MPI_Send(&sc, TAM_MAX, MPI_CHAR, ROOT, TAG, MPI_COMM_WORLD); 
-                        // MPI_Abort(MPI_COMM_WORLD, 0);
-                        // MPI_Finalize();
-                        // exit(0);
+                        MPI_Send(&senha, TAM_MAX, MPI_CHAR, ROOT, TAG, MPI_COMM_WORLD); 
                         break;
                     } 
                 }
             }
-
-            // printf("Process %d terminated\n", rank );
-
     }
 
     MPI_Finalize();
-
-
 
     return 0;
 }
