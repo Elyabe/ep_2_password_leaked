d55bcdb7f9139cb77109bf8939f3c6f0909775e7
elyabe <elly.ufes@gmail.com>
2019-07-05 19:55:57 -0300

    Adiçãodo caso teste 8 cores

--- run_ep2_8.sh
+++ run_ep2_8.sh
@@ -4,12 +4,12 @@
 #$ -j y
 #$ -S /bin/bash
 #$ -N elyabe
-#$ -pe mpich 2
+#$ -pe mpich 8
 
 # Redirecting standard output error to files
 # named "output" and "errors"
-#$ -o output_par_2.txt
-#$ -e errors_par_2.txt
+#$ -o output_par_8.txt
+#$ -e errors_par_8.txt
 
 senhas="902ba3cda1883801594b6e1b452790cc53948fda 8cb2237d0679ca88db6464eac60da96345513964 7b151de317f2547df39e1a1ff2850a6abfa6128f 230cdfcdb6f2aa33b7acf19edaae5a216a14155f b2cdbf0601d8ae90d3cda1c978566ace86c4eac0 a045b7efa463c6ed195c644163f4168952fbd34a";
 # senhas="8cb2237d0679ca88db6464eac60da96345513964";
@@ -19,6 +19,6 @@ for s in $senhas
 do
 	for (( i = 0; i < 3; i++ )) 
 	do
-		mpirun -np 2  --oversubscribe ./sha1  $s >> saida2.txt
+		mpirun -np 8  --oversubscribe ./sha1  $s >> saida2.txt
 	done
 done
