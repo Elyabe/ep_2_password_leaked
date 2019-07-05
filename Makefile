7aa896f0eea3a81554d043d9bc686b058c6b1e69
elyabe <elly.ufes@gmail.com>
2019-07-05 17:28:45 -0300

    Mensagem booleana seguida da senha

--- Makefile
+++ Makefile
@@ -10,9 +10,11 @@ sha1: $(OBJECTS)
 all:	sha1
 
 run:	all
-# 	mpirun -np 8 --oversubscribe ./sha1  902ba3cda1883801594b6e1b452790cc53948fda
-# 	mpirun -np 8 --oversubscribe ./sha1  4051c87045623d87ca8f6982fc8b2c971474bcb4
-	mpirun -np 8 --oversubscribe ./sha1  8cb2237d0679ca88db6464eac60da96345513964
+# 	mpirun -np 32 --oversubscribe ./sha1  902ba3cda1883801594b6e1b452790cc53948fda
+	mpirun -np 32 --oversubscribe ./sha1  4051c87045623d87ca8f6982fc8b2c971474bcb4
+# 	mpirun -np 32 --oversubscribe ./sha1  fc1aedf39e3245eb7aac3db3581ee786001f6479
+# 	mpirun -np 8 --oversubscribe ./sha1  8cb2237d0679ca88db6464eac60da96345513964
+# 	mpirun -np 4 --oversubscribe ./sha1  0776f15b676177c7c888da2d17e5f82a6e2a43d7
 
 
 clean:
--- main.cpp
+++ main.cpp
@@ -27,107 +27,106 @@ int main(int argc, char *argv[])
     }
 
     /* code */
-	char caracteres[] = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"; 
-    // char caracteres[] = "ae0123456789";    
-    // char caracteres[] = "abcdefghijklmnopqrstuvwxyz0123456789";    
-    char pwd_decrypt[255];
+    char caracteres[] = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", pwd_decrypt[255];
+    // char caracteres[] = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789", pwd_decrypt[255];
     strcpy(pwd_decrypt,argv[1]);
     
-   
-    long T = pow( strlen(caracteres), TAM_MAX), j, k, l, m, n, CH_LEN = strlen(caracteres), NUM_PROCESSOS = 10;
+    const long CH_LEN = strlen(caracteres);
     char sc[TAM_MAX+1];
-    sc[TAM_MAX] = '\0';
     
-    
-    int  rank, size, L = 1;
-    bool achou;
-    // const char p[] = "a100e";
-    // cout<< sha1(p) <<endl;
-    // exit(0);
+    int  rank, size, L = 1, done = -1;
+    bool achou, res;
 
     MPI_Status stats;
-
+    MPI_Request req;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
     MPI_Comm_size(MPI_COMM_WORLD, &size); 
 
-    achou = false;
+    res = achou = false;
 
-    /*if ( rank == ROOT )
+    if ( rank == ROOT )
     {
-        char recv[6];
-        
-        for ( k = 1; k < size ; ++k ) {
-            MPI_Recv(&achou, 1, MPI_C_BOOL, k, TAG, MPI_COMM_WORLD, &stats);
-            
-            if ( achou )
+        while( !res )
+        {
+            if(done != 0)
             {
-                printf("Processo %ld achou:\n", k);
-                MPI_Abort(MPI_COMM_WORLD, 0);
-                break;
-            }    
-        }
+                MPI_Irecv(&res, 1, MPI_C_BOOL, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &req);
+                done = 0;
+            }
+                MPI_Test(&req, &done, &stats);
 
-    } else */
+                if (done != 0) { 
+                    MPI_Recv(&sc, TAM_MAX, MPI_CHAR, stats.MPI_SOURCE, TAG, MPI_COMM_WORLD, &stats);
+                    
+                    printf("Processo : %d ==: senha %s \n", stats.MPI_SOURCE, sc);
+
+                    double tempoSeq = (MyClock()-inicio)/CLOCKS_PER_SEC;
+                    cout << fixed << setprecision(25);
+                    // cout << "  Valor esperado: " << M_PI << endl;
+                    // cout << "   Erro Relativo: " << fabs(x - M_PI)/M_PI << endl;
+                    printf("     Tempo total: %.5lf\n", tempoSeq);
+                    // cout << "   SpeedUp: " << 90.4/tempoSeq << endl;
+                    // cout << "   Eficiencia: " << 100*(90.4/tempoSeq)/16 << endl;
+
+                    done = -1;
+                    MPI_Abort(MPI_COMM_WORLD, 0);
+                    break;
+                }                
+
+        } 
+
+    } else 
     {
-            
-            achou = false;
+            long y[5], q, CH_LEN_POT[TAM_MAX+1];
+            long long QTD_POSSIB;
 
             for ( L = 1; !achou && L <= TAM_MAX; L++ )
             {
-                const long long T = (long long)pow( strlen(caracteres), L);
-                const long CH_LEN_POT[] = { 0, (long)pow(CH_LEN, L-1), (long)pow(CH_LEN, L-2), (long)pow(CH_LEN, L-3), CH_LEN };
-                printf("Processo %d L = %d \n", rank,  L );
-
-
+                QTD_POSSIB = (long long)pow( strlen(caracteres), L);
                 
-                // for ( long i = rank-1; i < T; i += size-1)
-                for ( long i = rank; i < T; i += size)
+                for ( q = 1; q <= TAM_MAX; q++ )
+                    CH_LEN_POT[q] = (long)pow(CH_LEN, L-q);
+                
+                // CH_LEN_POT[1] = (long)pow(CH_LEN, L-1);
+                // CH_LEN_POT[2] = (long)pow(CH_LEN, L-2);
+                // CH_LEN_POT[3] = (long)pow(CH_LEN, L-3);
+                // CH_LEN_POT[4] =  CH_LEN;
+                // CH_LEN_POT[5] =  1;
+
+                for ( long i = rank-1; i < QTD_POSSIB ; i += size-1)
                 {
-                    j = i / CH_LEN_POT[1];
-                    if ( L >= 2)
-                    {   
-                        k = i % CH_LEN_POT[1]  / CH_LEN_POT[2];
-                        
-                        if ( L >= 3 )
-                        {   
-                            l =  i % CH_LEN_POT[1] % CH_LEN_POT[2] / CH_LEN_POT[3];
-                            m =  i % CH_LEN_POT[1] % CH_LEN_POT[2] % CH_LEN_POT[3] / CH_LEN_POT[4];
-                            n =  i % CH_LEN_POT[1] % CH_LEN_POT[2] % CH_LEN_POT[3] % CH_LEN_POT[4]; 
-                        }
-                    }
+                    // Define os indices i,j,k,l
+                    y[0] = i;
+                    for ( q = 1 ; q < L; ++q )
+                        y[q] = y[q-1] % CH_LEN_POT[q];
 
-                    // printf("%d | %d | %d | %d | %d | %d  \n ", i,j,k,l,m, n);
+                    // Forma a palavra
+                    for ( q = 0 ; q < L; ++q )
+                        sc[q] = caracteres[y[q] / CH_LEN_POT[q+1]];    
+                    sc[q] = '\0';
 
-                    long y[] = { j,k,l,m,n }, z;
-                    
-                    for ( z = 0; z < L; ++z )
-                    {    
-                        sc[z] = caracteres[y[z]];
-                    }
-
-                    sc[z] = '\0';
-
-                    // printf("%s \n", sc );
 
                     if ( !strcmp( sha1(sc).c_str(), pwd_decrypt) )
                     {
-                        printf("%lf : Processo %d achou %s com L = %d \n", (MyClock()-inicio)/CLOCKS_PER_SEC,  rank, sc, L );
+                        // printf("%lf : Processo %d achou %s com L = %d \n", (MyClock()-inicio)/CLOCKS_PER_SEC,  rank, sc, L );
                         achou = true;
-                        // MPI_Bcast(&achou, 1, MPI_C_BOOL, rank, MPI_COMM_WORLD);
-                        MPI_Abort(MPI_COMM_WORLD, 0);
+
+                        // Informa ao raiz que achou
+                        MPI_Send(&achou, 1, MPI_C_BOOL, ROOT, TAG, MPI_COMM_WORLD); 
+                        // Envia a senha descriptografada
+                        MPI_Send(&sc, TAM_MAX, MPI_CHAR, ROOT, TAG, MPI_COMM_WORLD); 
+                        // MPI_Abort(MPI_COMM_WORLD, 0);
+                        // MPI_Finalize();
+                        // exit(0);
                         break;
-                    }    
-                    
+                    } 
                 }
-                
-                // MPI_Send(&achou, 1, MPI_C_BOOL, ROOT, TAG, MPI_COMM_WORLD); 
-                
             }
 
+            // printf("Process %d terminated\n", rank );
+
     }
-
-
 
     MPI_Finalize();
 
